bf7e40fa38b308fe83f04d925cf1e2e999e9998e
JuanRivalta <rivaltajuan90@gmail.com>
2025-11-11 15:50:13 -0300

    Update bmi.c
    Agregue nueva funcionalidad en el codigo de bmi para que ahora te diga tu condición en base a tu imc.

--- bmi.c
+++ bmi.c
@@ -9,10 +9,21 @@ int main(){
     scanf("%f", &altura);
 
     imc = peso / (altura*altura);
-
+    
     printf("\nSu indice de masa corporal es: %.2f", imc);
 
     printf("\nindice| Condicion\n-----------------------------\n<18.5 | Bajo peso\n18.5 a 24.9 | Normal\n25.0 a 29.9 | Sobrepeso\n>=30| Obesidad");
-
     
+    if (imc < 18.5){
+        printf("\nTu condicion es: Bajo peso");
+    }
+    else if (imc > 18.5 && imc < 25){
+        printf("\nTu condicion es: Normal");
+    }
+    else if (imc >= 25 && imc < 30){
+        printf("\nTu condicion es: Sobrepeso");
+    }
+    else if (imc >= 30){
+        printf("\nTu condicion es: Obesidad");
+    }
 }
